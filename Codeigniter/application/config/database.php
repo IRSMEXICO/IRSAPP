80a0952155024c00ce4f9f57044735d7951819f3
IRS DE MEXICO SA DE CV <55559158+IRSMEXICO@users.noreply.github.com>
2019-09-19 19:28:53 -0500

    s
    s

--- Codeigniter/application/config/database.php
+++ Codeigniter/application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '127.0.0.1',
+	'hostname' => 'https://irsdesarrollo.azurewebsites.net',
 	'username' => 'adminirs',
 	'password' => 'IRSDEMEXICO#2019',
 	'database' => 'irsbd',
