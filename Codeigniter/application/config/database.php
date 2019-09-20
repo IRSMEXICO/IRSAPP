6e6c2b36c6a5d29f9d7c124f80cefce579322bd1
IRS DE MEXICO SA DE CV <55559158+IRSMEXICO@users.noreply.github.com>
2019-09-19 19:11:38 -0500

    Configuracion config/database.php
    Configuracion config/database.php

--- Codeigniter/application/config/database.php
+++ Codeigniter/application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
+	'hostname' => '127.0.0.1',
+	'username' => 'adminirs',
+	'password' => 'IRSDEMEXICO#2019',
 	'database' => 'irsbd',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
