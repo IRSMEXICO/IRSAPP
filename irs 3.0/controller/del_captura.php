7dd8b3038c03b8cedd878a62353310caa9801139
Americo <americocardenasrochaa@gmail.com>
2019-09-24 14:24:51 -0500

    24/09/2019 irs 4.0

--- irs 3.0/controller/del_captura.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include ("../model/acciones.php");
-$id = $_GET['id'];
-echo $id;
-$tipo_act=new consul();
-$act_mod=$tipo_act->cat_del_captura($id);
-?>
--- irs 3.0/controller/del_moneda.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include ("../model/acciones.php");
-$id = $_GET['id'];
-echo $id;
-$tipo_moneda=new consul();
-$act_mon=$tipo_moneda->cat_del_moneda($id);
-?>
