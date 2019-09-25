d8c7018dba22af009849c773fddb65144379c3ef
Pacoesparza98 <48753361+Pacoesparza98@users.noreply.github.com>
2019-09-25 01:00:46 -0500

    Update conexion.php
    Actualizacion conexion

--- irs 3.0/model/conexion.php
+++ irs 3.0/model/conexion.php
@@ -1,7 +1,7 @@
 <?php
 class conexion{
     public function con(){
-        $link= mysqli_connect("irspruebas22019.mysql.database.azure.com", "irspruebas@irspruebas22019","IRSDEMEXICO#2019", "irsbd");
+        $link= mysqli_connect("irspruebas22019.mysql.database.azure.com", "irspruebas@irspruebas22019","IRSDEMEXICO#2019", "irs");
         $link->set_charset("utf8");
         return $link;
     }
