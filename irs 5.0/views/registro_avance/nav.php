03307850fa014696e2f4096aae4a89a6a4b6e4e3
Pacoesparza98 <48753361+Pacoesparza98@users.noreply.github.com>
2019-11-15 13:38:25 -0600

    Actualizacion nav reg_avance1 y 2

--- irs 5.0/views/registro_avance/nav.php
+++ irs 5.0/views/registro_avance/nav.php
@@ -66,9 +66,8 @@
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
           <a class="dropdown-item" href="admin.php">Admin</a>
           <a class="dropdown-item" href="inspector.php">Inspector</a>
-          <a class="dropdown-item" href="registro_avances.php">registro de Avances</a>
-          <a class="dropdown-item" href="registro_avances5.php">registro de Avances5</a>
-          <a class="dropdown-item" href="registro_avances3.php">Registro de avances 3</a>    
+          <a class="dropdown-item" href="reg_avance1.php">registro de Avances 1</a>
+          <a class="dropdown-item" href="reg_avance2.php">Registro de avances 2</a>    
         </div>
          </li>
           <!--**REGISTRO AVANCES**-->
