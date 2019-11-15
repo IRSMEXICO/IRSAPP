654c0e4d5754fec19f4dea47089bb1926af08fa7
Pacoesparza98 <48753361+Pacoesparza98@users.noreply.github.com>
2019-11-15 14:15:17 -0600

    Update admin.php

--- irs 5.0/views/admin.php
+++ irs 5.0/views/admin.php
@@ -78,10 +78,10 @@
         Registro de avances
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="registro_avances/admin.php">Admin</a>
-          <a class="dropdown-item" href="registro_avances/inspector.php">Inspector</a>
-          <a class="dropdown-item" href="registro_avances/reg_avance1.php">registro de Avances</a>
-          <a class="dropdown-item" href="registro_avances/reg_avance2.php">registro de Avances2</a>
+          <a class="dropdown-item" href="registro_avance/admin.php">Admin</a>
+          <a class="dropdown-item" href="registro_avance/inspector.php">Inspector</a>
+          <a class="dropdown-item" href="registro_avance/reg_avance1.php">registro de Avances</a>
+          <a class="dropdown-item" href="registro_avance/reg_avance2.php">registro de Avances2</a>
         </div>
          </li>
           <!--**REGISTRO AVANCES**-->
