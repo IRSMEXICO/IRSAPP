3d693b59f530e186ec06aa8f188e1011914b7942
Pacoesparza98 <48753361+Pacoesparza98@users.noreply.github.com>
2019-11-15 13:46:30 -0600

    Act reg avances

--- irs 5.0/views/registro_avance/nav.php
+++ irs 5.0/views/registro_avance/nav.php
@@ -64,10 +64,11 @@
         Registro de avances
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="admin.php">Admin</a>
-          <a class="dropdown-item" href="inspector.php">Inspector</a>
-          <a class="dropdown-item" href="reg_avance1.php">registro de Avances 1</a>
-          <a class="dropdown-item" href="reg_avance2.php">Registro de avances 2</a>    
+          <a class="dropdown-item" href="registro_avances/admin.php">Admin</a>
+          <a class="dropdown-item" href="registro_avances/inspector.php">Inspector</a>
+          <a class="dropdown-item" href="registro_avances/registro_avances.php">registro de Avances</a>
+          <a class="dropdown-item" href="registro_avances/registro_avances1.php">registro de Avances2</a>
+          <a class="dropdown-item" href="registro_avances/registro_avances2.php">Registro de avances 3</a>      
         </div>
          </li>
           <!--**REGISTRO AVANCES**-->
