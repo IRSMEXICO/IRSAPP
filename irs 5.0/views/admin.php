b9fb0067819a94c89b0e7498ee9d122da488a6fa
AmericoCardenas <42499567+AmericoCardenas@users.noreply.github.com>
2019-11-23 18:53:01 -0600

    menu admin
    solucion error etiqueta nivo slider en head

--- irs 5.0/views/admin.php
+++ irs 5.0/views/admin.php
@@ -15,7 +15,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!--SLIDER-->
         <link rel="stylesheet" href="../content/css/nivo-slider.css">
-	      <link rel="stylesheet" href="<../content/css/mi-slider.css">
+	      <link rel="stylesheet" href="../content/css/mi-slider.css">
 	      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js" type="text/javascript"></script>
   	    <script src="../content/js/jquery.nivo.slider.js"></script>
 	      <script type="text/javascript"> 
